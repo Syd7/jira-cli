bec7aef10bb094b1f586e0e6845f7869b7ba2b94
Hamza Sayadi (ext.) <Hamza.Sayadi@partner.bmwgroup.com>
2022-10-04 23:45:52 +0100

    feat: add weblink to issue (#466)

---
+++ pkg/jira/remotelink.go
@@ -0,0 +1,45 @@
+package jira
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type weblinkRequest struct {
+	RemoteObject struct {
+		Url   string `json:"url"`
+		Title string `json:"title"`
+	} `json:"object"`
+}
+
+// Add a web link to an issue using POST /issue/{issueId}/remotelink endpoint.
+func (c *Client) WebLinkIssue(issueId, title, url string) error {
+	body, err := json.Marshal(weblinkRequest{
+		RemoteObject: struct {
+			Url   string `json:"url"`
+			Title string `json:"title"`
+		}{Title: title, Url: url},
+	})
+	if err != nil {
+		return err
+	}
+
+	res, err := c.PostV2(context.Background(), fmt.Sprintf("/issue/%s/remotelink/", issueId), body, Header{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	})
+	if err != nil {
+		return err
+	}
+	if res == nil {
+		return ErrEmptyResponse
+	}
+	defer func() { _ = res.Body.Close() }()
+
+	if res.StatusCode != http.StatusCreated {
+		return formatUnexpectedResponse(res)
+	}
+	return nil
+}
